16f98afb930160dacbb99372e04056cb1b52e7d9
minio999 <minkowskidominik69@gmail.com>
2022-06-06 18:42:23 +0200

    Init Files

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("LICENSE") as file:
+    license = file.read() 
+
+
+setup(
+    name="app",
+    version='0.1.0',
+    description='Predicting house prices',
+    author='Dominik Mińkowski',
+    author_email='minkowskidominik03@gmail.com',
+    url='',
+    license=license,
+    include_package_data=True,
+    package_dir={"":"src"},
+    packages=find_packages(where='src'),
+)
