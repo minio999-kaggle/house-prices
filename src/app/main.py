5d1ab899b88e82d9e3faee3e5c0461bca164df14
minio999 <minkowskidominik69@gmail.com>
2022-06-15 22:08:08 +0200

    Added model

---
+++ src/app/main.py
@@ -0,0 +1,50 @@
+'''
+Main module for application.
+'''
+from sklearn.model_selection import KFold
+from catboost import CatBoostRegressor
+from sklearn.metrics import mean_squared_error
+import numpy as np
+from .preprocessing import get_df
+
+LABEL = 'SalePrice'
+
+def main():
+    '''
+    Main Function
+    '''
+    df = get_df()
+
+    X = df.drop(['SalePrice', 'Id'], axis=1)
+    y = df[LABEL]
+
+    k_fold = KFold(
+    n_splits=5,
+    shuffle=True,
+    random_state=42
+    )
+
+    scores = []
+    for train_index, test_index in k_fold.split(X):
+        X_train, X_test = X.loc[train_index], X.loc[test_index]
+        y_train, y_test = y.loc[train_index], y.loc[test_index]
+
+        reg = CatBoostRegressor()
+
+        reg.fit(X_train, y_train)
+
+        y_predict = reg.predict(X_test)
+
+        acc_score = np.sqrt(mean_squared_error(y_test, y_predict))
+        
+        print(acc_score)
+
+        scores.append(acc_score)
+
+    print()
+    print("Average:", np.sum(scores)/5)
+    print("Std:", round(np.std(scores), 3))
+
+if __name__  == '__main__':
+    main()
+    print("Done")
